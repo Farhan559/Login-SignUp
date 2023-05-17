94d14c8b86e54c38c3787d059bd2f74cdddd0b90
Farhan559 <farhansiddiqui559@gmail.com>
2023-05-17 14:07:41 +0500

    Login SignUp Form Using HTML CSS

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+function sub(){
+
+var name = document.getElementById('name')
+var password= document.getElementById('password')
+
+    document.write(name.innerHTML);
+    document.write(password.innerHTML);
+}
+
+
